66a25b15f7ec42c3534ef1d84ad531f43f11751f
Briana <briana_brewster4@yahoo.com>
2022-11-01 15:16:44 -0500

    commit tree 5659bd0fe4529df9f867a0f2dcb7917ceb83db26

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+    validate :clickbait
+
+
+    def clickbait
+
+        if title == nil
+            return 
+        end
+
+    clickbait_words = [
+        "Won't Believe",
+        "Secret",
+        "Top [number]",
+        "Guess"
+    ]
+        if clickbait_words.none? {|c| title.include?(c) }
+            errors.add(:title, "Not clickbait-y enough!")
+        end
+    end
 end
